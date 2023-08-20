67661346c0a2482dd3691efe5c44e178455d2457
Zach Robles <zrobles1188@gmail.com>
2023-08-20 12:44:06 -0400

    one working example

--- Practice Problem/src/findOccurrences.js
+++ Practice Problem/src/findOccurrences.js
@@ -6,6 +6,29 @@
   Return:  
     an array of all sentences containing words in the list
 */
-function findOccurrences(words, index, data) {}
+function findOccurrences(words, index, data) {
+  // declare uniqueIndexesOfWords new Set()
+  // declare words.head into variable wordZ
+  // while wodrZ.next
+
+  // index[wordZ.value].forEach((indexOfWord)=>{
+  // uniqueIndexesOfWords.add(data[indexOfWord])
+  // })
+
+  //
+
+  const uniqueIndexesOfWords = new Set();
+  let wordHolder = words.head;
+  while (wordHolder) {
+    if (index[wordHolder.value]) {
+      index[wordHolder.value].forEach((indexOfWord) => {
+        uniqueIndexesOfWords.add(data[indexOfWord]);
+      });
+    }
+    wordHolder = wordHolder.next;
+  }
+  console.log(uniqueIndexesOfWords);
+  return Array.from(uniqueIndexesOfWords);
+}
 
 module.exports = findOccurrences;
